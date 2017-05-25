296ea854691778af2ebb24b110ff960ea28e6fe2
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-25 19:41:50 +0300

    Modified activity_signup

--- app/src/main/java/com/example/kiragu/flowershopfinder/MainActivity.java
+++ app/src/main/java/com/example/kiragu/flowershopfinder/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.kiragu.flowershopfinder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
@@ -15,5 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        mSignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {;
+                Intent intent = new Intent(MainActivity.this, Sign_Up.class);
+                startActivity(intent);
+            }
+        });
     }
 }
