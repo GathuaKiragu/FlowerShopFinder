9412b6239d1d748301940e4c68a0e60f927fd17b
Gathua Kiragu <kiragugathua@gmail.com>
2017-05-25 18:35:14 +0300

    Added butternife dependency

--- app/src/main/java/com/example/kiragu/flowershopfinder/MainActivity.java
+++ app/src/main/java/com/example/kiragu/flowershopfinder/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.kiragu.flowershopfinder;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.signUpButton) Button mSignUpButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
